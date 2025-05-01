Open-Email/OpenEmail-GTK

commit 85dc8a07c0e0ca2c250d4ad15a40231c699fa476

mail: Add the ability to empty the trash

openemail/mail.py
@@ -328,22 +328,30 @@ async def discard_message(message: Message) -> None:
     await outbox.update()
 
 
-def trash_message(message_id: str) -> None:
-    """Move the message associated with `message_id` to the trash."""
+def trash_message(ident: str) -> None:
+    """Move the message associated with `ident` to the trash."""
     settings.set_strv(
         "trashed-messages",
-        settings.get_strv("trashed-messages") + [message_id],
+        tuple(set(settings.get_strv("deleted-messages")) | {ident}),
     )
 
 
-def restore_message(message_id: str) -> None:
-    """Restore the message associated with `message_id` from the trash."""
+def restore_message(ident: str) -> None:
+    """Restore the message associated with `ident` from the trash."""
     try:
-        (trashed := settings.get_strv("trashed-messages")).remove(message_id)
+        (trashed := settings.get_strv("trashed-messages")).remove(ident)
     except ValueError:
         return
 
     settings.set_strv("trashed-messages", trashed)
+
+
+def empty_trash() -> None:
+    """Empty the user's trash."""
+    for store in inbox, broadcasts:
+        for message in store:
+            if message.trashed:  # type: ignore
+                store.delete(message.message.message_id)  # type: ignore
 
 
 def log_out() -> None:
@@ -360,8 +368,10 @@ def log_out() -> None:
 
     settings.reset("address")
     settings.reset("sync-interval")
+    settings.reset("trusted-domains")
     settings.reset("contact-requests")
     settings.reset("trashed-messages")
+    settings.reset("deleted-messages")
 
     keyring.delete_password(secret_service, str(user.address))
 
@@ -504,7 +514,19 @@ class MailMessageStore(DictStore[str, MailMessage]):
         self.item_type = MailMessage
 
     @abstractmethod
-    async def _fetch(self): ...  # noqa: ANN202
+    async def _fetch(self) -> ...: ...
+
+    def delete(self, message_id: str) -> None:
+        """Delete the message with `message_id`.
+
+        From the user's perspective, this means removing an item from the trash.
+        """
+        settings.set_strv(
+            "deleted-messages",
+            tuple(set(settings.get_strv("deleted-messages")) | {message_id}),
+        )
+        self.remove(message_id)
+        restore_message(message_id)
 
     @_syncs
     async def update(self) -> None:
@@ -788,8 +810,9 @@ class MailContactRequests(MailProfileStore):
 
 class _BroadcastStore(MailMessageStore):
     async def _fetch(self) -> AsyncGenerator[Message, None]:  # type: ignore
+        deleted = settings.get_strv("deleted-messages")
         async for messages in asyncio.as_completed(
-            client.fetch_broadcasts(Address(contact.address))  # type: ignore
+            client.fetch_broadcasts(Address(contact.address), exclude=deleted)  # type: ignore
             for contact in address_book
         ):
             for message in await messages:
@@ -819,10 +842,17 @@ class _InboxStore(MailMessageStore):
 
                 settings.set_strv("contact-requests", current + [str(notifier)])
 
+        deleted = settings.get_strv("deleted-messages")
         async for messages in asyncio.as_completed(
             (
-                *(client.fetch_link_messages(n) for n in known_notifiers),
-                *(client.fetch_link_messages(contact) for contact in other_contacts),
+                *(
+                    client.fetch_link_messages(n, exclude=deleted)
+                    for n in known_notifiers
+                ),
+                *(
+                    client.fetch_link_messages(contact, exclude=deleted)
+                    for contact in other_contacts
+                ),
             )
         ):
             for message in await messages:

data/gschema.xml.in
@@ -16,6 +16,9 @@
 		<key name="trashed-messages" type="as">
 			<default>[]</default>
 		</key>
+		<key name="deleted-messages" type="as">
+			<default>[]</default>
+		</key>
 	</schema>
 	<schema id="@APP_ID@.State" path="@PREFIX@/State/">
 		<key name="width" type="i">

openemail/core/client.py
@@ -562,11 +562,17 @@ async def fetch_message_ids(author: Address, broadcasts: bool = False) -> set[st
 
 
 async def fetch_messages(
-    author: Address, *, broadcasts: bool = False
+    author: Address,
+    *,
+    broadcasts: bool = False,
+    exclude: Iterable[str] = (),
 ) -> tuple[Message, ...]:
     """Fetch either link messages or broadcasts by `author`."""
     messages: dict[str, Message] = {}
     for message_id in await fetch_message_ids(author, broadcasts=broadcasts):
+        if message_id in exclude:
+            continue
+
         for agent in await get_agents(user.address):
             if message := await fetch_message_from_agent(
                 (
@@ -594,16 +600,20 @@ async def fetch_messages(
     return tuple(messages.values())
 
 
-async def fetch_broadcasts(author: Address) -> tuple[Message, ...]:
-    """Fetch broadcasts by `author`."""
+async def fetch_broadcasts(
+    author: Address, *, exclude: Iterable[str] = ()
+) -> tuple[Message, ...]:
+    """Fetch broadcasts by `author`, without messages with IDs in `exclude`."""
     logging.debug("Fetching broadcasts from %s…", author)
-    return await fetch_messages(author, broadcasts=True)
+    return await fetch_messages(author, broadcasts=True, exclude=exclude)
 
 
-async def fetch_link_messages(author: Address) -> tuple[Message, ...]:
-    """Fetch messages by `author`, addressed to `client.user`."""
+async def fetch_link_messages(
+    author: Address, *, exclude: Iterable[str] = ()
+) -> tuple[Message, ...]:
+    """Fetch messages by `author`, addressed to `client.user`, without messages with IDs in `exclude`."""
     logging.debug("Fetching link messages messages from %s…", author)
-    return await fetch_messages(author)
+    return await fetch_messages(author, exclude=exclude)
 
 
 async def download_attachment(parts: Iterable[Message]) -> bytes | None:

openemail/gtk/messages-page.blp
@@ -39,3 +39,16 @@ template $MailMessagesPage: Adw.NavigationPage {
     };
   }
 }
+
+Adw.AlertDialog confirm_empty_dialog {
+  heading: _("Empty Trash?");
+  body: _("All items in the trash will be permanently deleted.");
+  close-response: "cancel";
+  default-response: "empty";
+  response => $_confirm_empty();
+
+  responses [
+    cancel: _("Cancel"),
+    empty: _("Empty Trash") destructive,
+  ]
+}

openemail/widgets/messages_page.py
@@ -25,7 +25,7 @@ from typing import Any, Literal
 from gi.repository import Adw, Gio, GObject, Gtk
 
 from openemail import PREFIX, mail, settings
-from openemail.mail import MailMessage
+from openemail.mail import MailMessage, empty_trash
 
 from .compose_dialog import MailComposeDialog
 from .content_page import MailContentPage
@@ -41,12 +41,20 @@ class MailMessagesPage(Adw.NavigationPage):
     content: MailContentPage = Gtk.Template.Child()
     message_view: MailMessageView = Gtk.Template.Child()
 
+    confirm_empty_dialog = Gtk.Template.Child()
+
     title = GObject.Property(type=str, default=_("Messages"))
     compose_dialog = GObject.Property(type=MailComposeDialog)
 
     _folder: str | None = None
 
-    @GObject.Property(type=str)
+    @GObject.Property(
+        type=str,
+        flags=GObject.PARAM_STATIC_STRINGS
+        | GObject.PARAM_READABLE
+        | GObject.PARAM_WRITABLE
+        | GObject.PARAM_CONSTRUCT_ONLY,
+    )
     def folder(self) -> str | None:
         """Get the folder this page represents."""
         return self._folder
@@ -69,6 +77,14 @@ class MailMessagesPage(Adw.NavigationPage):
                 inboxes.append(mail.broadcasts)
                 inboxes.append(mail.inbox)
                 model = Gtk.FlattenListModel.new(inboxes)
+
+                (
+                    empty_button := Gtk.Button(
+                        icon_name="trash-symbolic",
+                        tooltip_text=_("Empty Trash"),
+                    )
+                ).connect("clicked", lambda *_: self.confirm_empty_dialog.present(self))
+                self.content.toolbar_button = empty_button
 
         self.content.model = Gtk.SingleSelection(
             autoselect=False,
@@ -127,6 +143,13 @@ class MailMessagesPage(Adw.NavigationPage):
         super().__init__(**kwargs)
 
         self.message_view.reply_button.connect("clicked", self._reply)
+
+    @Gtk.Template.Callback()
+    def _confirm_empty(self, _obj: Any, response: str) -> None:
+        if response != "empty":
+            return
+
+        empty_trash()
 
     @Gtk.Template.Callback()
     def _new_message(self, *_args: Any) -> None:

commit 2e420fa6639b4aee255b283223cc0eaaa40204ef

mail: Fix trashing messages
Broken since 85dc8a07c0e0ca2c250d4ad15a40231c699fa476

openemail/mail.py
@@ -332,7 +332,7 @@ def trash_message(ident: str) -> None:
     """Move the message associated with `ident` to the trash."""
     settings.set_strv(
         "trashed-messages",
-        tuple(set(settings.get_strv("deleted-messages")) | {ident}),
+        tuple(set(settings.get_strv("trashed-messages")) | {ident}),
     )
 
 